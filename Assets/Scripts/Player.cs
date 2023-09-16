ab882d933865525de64d24f2512c00701a4323e4
Chloe Lee-Hone <chloe.leehone@gmail.com>
2023-09-16 15:47:07 -0400

    Add Player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Serializes the data to allow a private variable to be visible and overridable in the Unity editor
+    [SerializeField] 
+    private int _speed = 5;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Time.deltaTime incorporates real time. Distributed property Vector3(1, 0, 0) * 5 * real time --> equals new Vector3(5, 0, 0)
+        transform.Translate(Vector3.right * _speed * Time.deltaTime);
     }
 }
