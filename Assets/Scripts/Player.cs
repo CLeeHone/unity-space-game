3761e83ec431efa8348a59425cb52ac86638f7a4
Chloe Lee-Hone <chloe.leehone@gmail.com>
2023-09-17 12:09:53 -0400

    Refactor Player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Player : MonoBehaviour
 {
+    public const float RightLimit = 11.3f;
+    public const float LeftLimit = -11.3f;
+    public const float UpperLimit = 0f;
+    public const float LowerLimit = -3.8f;
+
     // Serializes the data to allow a private variable to be visible and overridable in the Unity editor
     [SerializeField] 
     private int _speed = 5;
@@ -17,6 +23,11 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         float forwardInput = 0f;
@@ -24,5 +35,15 @@ public class Player : MonoBehaviour
 
         // Time.deltaTime incorporates real time. Distributed property Vector3(1, 0, 0) * 5 * real time --> equals new Vector3(5, 0, 0)
         transform.Translate(direction * _speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, LowerLimit, UpperLimit), transform.position.z);
+
+        if (transform.position.x >= RightLimit)
+        {
+            transform.position = new Vector3(LeftLimit, transform.position.y, transform.position.z);
+        } else if (transform.position.x <= LeftLimit)
+        {
+            transform.position = new Vector3(RightLimit, transform.position.y, transform.position.z);
+        }
     }
 }
