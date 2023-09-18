f1165a0bca452665885152e0af6640d0a57666ab
Chloe Lee-Hone <chloe.leehone@gmail.com>
2023-09-18 13:37:13 -0400

    Add Enemy behaviour and movement

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3f;
+    public const float RightLimit = 9.48f;
+    public const float LeftLimit = -9.48f;
+    public const float UpperLimit = 8f;
+    public const float LowerLimit = -5.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, UpperLimit, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if (transform.position.y < LowerLimit)
+        {
+            transform.position = new Vector3(Random.Range(LeftLimit, RightLimit), UpperLimit, transform.position.z);
+        }
+
+    }
+}
