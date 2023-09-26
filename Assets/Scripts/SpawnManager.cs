1abf5c54e76f86d76a3991a07e7214358d23a40b
Chloe Lee-Hone <chloe.leehone@gmail.com>
2023-09-26 12:28:49 -0400

    Add SpawnManager and spawning behaviour

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    private IEnumerator _coroutine;
+    [SerializeField]
+    private GameObject _enemyPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            Enemy enemy = gameObject.AddComponent(typeof(Enemy)) as Enemy;
+            Vector3 newPosition = new Vector3(Random.Range(Enemy.LeftLimit, Enemy.RightLimit), Enemy.UpperLimit, 0);
+            Instantiate(_enemyPrefab, newPosition, Quaternion.identity);
+
+            yield return new WaitForSeconds(5);
+        }
+    }
+}
